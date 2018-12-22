085caef1a031c2e6fdf4e90afce9dcd78729899e
Martin Atkins <mart@degeneration.co.uk>
2018-12-22 23:47:57 +0000

    Complete the Windows ToURL and FromURL methods

--- paths/windows.go
+++ paths/windows.go
@@ -1,6 +1,8 @@
 package paths
 
 import (
+	"errors"
+	"fmt"
 	"net/url"
 	"strings"
 )
@@ -78,18 +80,68 @@ func (im impl) windowsJoinNonEmpty(elem []string) string {
 func (im impl) windowsToURL(path string) *url.URL {
 	// We always produce the "standard" convention for file: URLs on Windows
 	// as understood by the Windows shell and Internet Explorer:
-	u := &url.URL{
-		Path: im.Clean(path),
+	// https://blogs.msdn.microsoft.com/ie/2006/12/06/file-uris-in-windows/
+
+	if !im.IsAbs(path) {
+		// A relative path becomes a schemeless URL.
+		return &url.URL{
+			Path: im.toSlash(path),
+		}
 	}
-	// For an absolute path we also set the scheme.
-	if im.IsAbs(path) {
-		u.Scheme = "file"
+
+	if im.isUNC(path) {
+		// For UNC paths, the hostname part goes in the hostname part of
+		// the file URI:
+		//  \\foo\bar\baz
+		//  file://foo/bar/baz
+		volLen := im.volumeNameLen(path)
+		return &url.URL{
+			Scheme: "file",
+			Host:   path[2:volLen],
+			Path:   path[volLen:],
+		}
 	}
-	return u
+
+	// Absolute non-UNC (i.e. drive-lettered) paths become file: URLs with
+	// an empty hostname part.
+	return &url.URL{
+		Scheme: "file",
+		Path:   im.toSlash(path),
+	}
 }
 
 func (im impl) windowsFromURL(u *url.URL) (string, error) {
-	return "", nil
+	// There are lots of weird legacy forms of file: URI that are accepted
+	// on Windows systems. This is not a faithful bug-compatible implementation,
+	// but we do try to honor some of the more common misguided forms.
+
+	switch u.Scheme {
+	case "":
+		// Relative URL
+		return im.Clean(u.Path), nil
+	case "file":
+		// We'll tolerate and ignore query and fragment parts
+		if u.User != nil {
+			return "", errors.New("user portion not allowed in file: URLs")
+		}
+		if u.Host != "" {
+			// If a hostname is present then it's a UNC path.
+			return im.Clean(fmt.Sprintf("\\%s%s", u.Host, u.Path)), nil
+		}
+		// Otherwise the first part of the path ought to be a drive letter
+		// followed by an unescaped colon.
+		p := u.Path
+		if len(p) >= 3 && p[2] == '|' {
+			// Legacy Netscape form with pipe instead of colon
+			p = fmt.Sprintf("%s:%s", p[:2], p[3:])
+		}
+		if len(p) < 4 || p[0] != '/' || p[2] != ':' {
+			return "", errors.New("local file: URLs must begin with a drive letter and then a colon in the path portion")
+		}
+		return im.Clean(p[1:]), nil // trim off leading slash
+	default:
+		return "", errors.New("file: is the only allowed URL scheme")
+	}
 }
 
 func (im impl) isUNC(path string) bool {
