ca7693264a23e34e2e3347bb72c04cb61835958b
Martin Atkins <mart@degeneration.co.uk>
2018-12-28 01:38:18 +0000

    More elaborate package documentation

--- paths/doc.go
+++ paths/doc.go
@@ -11,4 +11,27 @@
 // This package includes only functions that do not require access to a real
 // filesystem. However, paths returned by methods of paths.Target can be used
 // with the path/filepath package functions for that additional functionality.
+//
+// OS-Specific Paths
+//
+// The main entrypoints to this package are the variables Unix and Windows,
+// which have methods with the same names and behaviors as those in the
+// path/filepath package, and behave as that package would on the respective
+// GOOS.
+//
+// The variable Target refers to either Unix or Windows depending on GOOS,
+// and thus it effectively provides aliases for a subset of the functions
+// of path/filepath.
+//
+// Slash Paths
+//
+// The variable Slash is a wrapper around the "path" package for handling
+// slash-based paths as seen in URLs.
+//
+// Other Implementations
+//
+// Interface P is the type of all of the different path implementations in this
+// package. Other packages can potentially implement this interface; although
+// it has a large number of methods, the methodset will not change until a
+// hypothetical major version 2 of this package.
 package paths
