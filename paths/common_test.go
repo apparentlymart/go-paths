cb295c6d89e2c4579d71ea50cee3229f84c2c4c4
Martin Atkins <mart@degeneration.co.uk>
2018-12-28 00:41:44 +0000

    Tests for "Join"

--- paths/common_test.go
+++ paths/common_test.go
@@ -168,3 +168,87 @@ func TestSplit(t *testing.T) {
 		})
 	}
 }
+
+func TestJoin(t *testing.T) {
+	type Test struct {
+		elem []string
+		path string
+	}
+
+	implTests := map[string][]Test{
+		"Unix": {
+			// zero parameters
+			{[]string{}, ""},
+
+			// one parameter
+			{[]string{""}, ""},
+			{[]string{"/"}, "/"},
+			{[]string{"a"}, "a"},
+
+			// two parameters
+			{[]string{"a", "b"}, "a/b"},
+			{[]string{"a", ""}, "a"},
+			{[]string{"", "b"}, "b"},
+			{[]string{"/", "a"}, "/a"},
+			{[]string{"/", "a/b"}, "/a/b"},
+			{[]string{"/", ""}, "/"},
+			{[]string{"//", "a"}, "/a"},
+			{[]string{"/a", "b"}, "/a/b"},
+			{[]string{"a/", "b"}, "a/b"},
+			{[]string{"a/", ""}, "a"},
+			{[]string{"", ""}, ""},
+
+			// three parameters
+			{[]string{"/", "a", "b"}, "/a/b"},
+		},
+		"Windows": {
+			{[]string{`directory`, `file`}, `directory\file`},
+			{[]string{`C:\Windows\`, `System32`}, `C:\Windows\System32`},
+			{[]string{`C:\Windows\`, ``}, `C:\Windows`},
+			{[]string{`C:\`, `Windows`}, `C:\Windows`},
+			{[]string{`C:`, `a`}, `C:a`},
+			{[]string{`C:`, `a\b`}, `C:a\b`},
+			{[]string{`C:`, `a`, `b`}, `C:a\b`},
+			{[]string{`C:`, ``, `b`}, `C:b`},
+			{[]string{`C:`, ``, ``, `b`}, `C:b`},
+			{[]string{`C:`, ``}, `C:.`},
+			{[]string{`C:`, ``, ``}, `C:.`},
+			{[]string{`C:.`, `a`}, `C:a`},
+			{[]string{`C:a`, `b`}, `C:a\b`},
+			{[]string{`C:a`, `b`, `d`}, `C:a\b\d`},
+			{[]string{`\\host\share`, `foo`}, `\\host\share\foo`},
+			{[]string{`\\host\share\foo`}, `\\host\share\foo`},
+			{[]string{`//host/share`, `foo/bar`}, `\\host\share\foo\bar`},
+			{[]string{`\`}, `\`},
+			{[]string{`\`, ``}, `\`},
+			{[]string{`\`, `a`}, `\a`},
+			{[]string{`\\`, `a`}, `\a`},
+			{[]string{`\`, `a`, `b`}, `\a\b`},
+			{[]string{`\\`, `a`, `b`}, `\a\b`},
+			{[]string{`\`, `\\a\b`, `c`}, `\a\b\c`},
+			{[]string{`\\a`, `b`, `c`}, `\a\b\c`},
+			{[]string{`\\a\`, `b`, `c`}, `\a\b\c`},
+		},
+	}
+
+	implTests["Windows"] = append(implTests["Windows"], implTests["Unix"]...)
+
+	impls := map[string]P{
+		"Unix":    Unix,
+		"Windows": Windows,
+	}
+
+	for implName, tests := range implTests {
+		t.Run(implName, func(t *testing.T) {
+			im := impls[implName]
+			for _, test := range tests {
+				t.Run(test.path, func(t *testing.T) {
+					expected := im.(impl).fromSlash(test.path)
+					if p := im.Join(test.elem...); p != expected {
+						t.Errorf("Join(%q) = %q, want %q", test.elem, p, expected)
+					}
+				})
+			}
+		})
+	}
+}
