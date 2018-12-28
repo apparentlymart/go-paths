682fe11eb7602a7c2bca61f1a314e0fa3d42fb66
Martin Atkins <mart@degeneration.co.uk>
2018-12-28 01:43:35 +0000

    A few more examples

--- paths/examples_test.go
+++ paths/examples_test.go
@@ -6,6 +6,53 @@ import (
 	"github.com/apparentlymart/go-paths/paths"
 )
 
+func Example() {
+	dir := paths.Windows.Dir(`c:\windows\system32\shell32.dll`)
+	fmt.Println(paths.Windows.Join(dir, "../explorer.exe"))
+
+	// Output:
+	// c:\windows\explorer.exe
+}
+
+func ExampleUnix() {
+	start := "/home/fred/.config/bar/baz"
+	dir, fn := paths.Unix.Split(start)
+	fmt.Println(dir, fn)
+	fmt.Println(paths.Unix.Join(dir, fn))
+	fmt.Println(paths.Unix.Join(dir, "boz"))
+
+	// Output:
+	// /home/fred/.config/bar/ baz
+	// /home/fred/.config/bar/baz
+	// /home/fred/.config/bar/boz
+}
+
+func ExampleWindows() {
+	start := `c:\windows\system32\shell32.dll`
+	dir, fn := paths.Windows.Split(start)
+	fmt.Println(dir, fn)
+	fmt.Println(paths.Windows.Join(dir, fn))
+	fmt.Println(paths.Windows.Join(dir, "moricons.dll"))
+
+	// Output:
+	// c:\windows\system32\ shell32.dll
+	// c:\windows\system32\shell32.dll
+	// c:\windows\system32\moricons.dll
+}
+
+func ExampleSlash() {
+	start := "/articles/go-patterns.html"
+	dir, fn := paths.Slash.Split(start)
+	fmt.Println(dir, fn)
+	fmt.Println(paths.Slash.Join(dir, fn))
+	fmt.Println(paths.Slash.Join(dir, "go-antipatterns.html"))
+
+	// Output:
+	// /articles/ go-patterns.html
+	// /articles/go-patterns.html
+	// /articles/go-antipatterns.html
+}
+
 func ExampleP_Split() {
 	fmt.Println(paths.Unix.Split("/foo/bar"))
 	fmt.Println(paths.Windows.Split("c:/foo/bar"))
